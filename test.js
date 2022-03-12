fce7b4daa7077fb1a80db369072f869bf012bf08
edoardottt <edoardott@gmail.com>
2022-03-12 16:34:32 +0100

    update test (ipv6 with port)

--- test.js
+++ test.js
@@ -62,3 +62,7 @@ console.log("[ SHORT IPv4 ]:    " + defangjs.defangIp("127.1:80") + "\n");
 console.log("[ IPv6 ]:          " + defangjs.defangIp("2001:0db8:85a3:0000:0000:8a2e:0370:7334") + "\n");
 
 console.log("[ SHORT IPv6 ]:    " + defangjs.defangIp("2001:0db8:85a3::8a2e:0370:7334") + "\n");
+
+console.log("[ IPv6 ]:          " + defangjs.defangIp("[2001:0db8:85a3:0000:0000:8a2e:0370:7334]:8080") + "\n");
+
+console.log("[ SHORT IPv6 ]:    " + defangjs.defangIp("[2001:0db8:85a3::8a2e:0370:7334]:8080") + "\n");
